e3b968cb5aa3d56d888bb39cc8822cb317e136ef
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2024-03-26 17:28:05 +0000

    implement jwt

--- src/routes/userAuthentication/routes.ts
+++ src/routes/userAuthentication/routes.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { app, handleError } from '../..';
 import { authenticationHandler } from './handlers/authenticationHandler';
 import { LoginProviderType } from '../../repository/types';
+import { createToken, verifyToken } from '../../jwtService';
 
 /**
  * Authenticate an user with a login provider
@@ -15,8 +16,29 @@ app.post('/:appId/authenticate/:providerId', async (req, res) => {
     const appId = req.params.appId.toLowerCase();
     const providerId = z.nativeEnum(LoginProviderType).parse(req.params.providerId.toLowerCase());
     const user = await authenticationHandler(code, appId, providerId, redirectUrl);
-    res.json(user);
+
+    const token = await createToken(user);
+
+    res.json({
+      user,
+      token
+    });
   } catch(err) {
+    handleError(err, req, res);
+  }
+});
+
+app.post('/token/verify', async (req, res) => {
+  try {
+    const { token } = z.object({
+      token: z.string(),
+    }).parse(req.body);
+    const result = await verifyToken(token);
+    if (!result) {
+      return res.status(403).send();
+    }
+    return res.status(200).send();
+  } catch (err) {
     handleError(err, req, res);
   }
 });
