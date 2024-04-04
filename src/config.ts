4fbc38c41cd7a230b3ec7827c5eea21a9ee17d8b
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2024-04-04 19:16:14 +0100

    fixed cron

--- src/config.ts
+++ src/config.ts
@@ -16,6 +16,6 @@ if (
 export const TOKEN_REFRESH_SETTINGS = {
   // At the start of every 1th hour from 12am through 11pm
   // https://cron.help/#0_0/1_*_*_*
-  updateInterval: '0 0/1 * * *', // every 1 hours
+  updateInterval: '0 * * * *', // every 1 hours
   updateThreshold: 7200000 // 2 hours before expires
 };
